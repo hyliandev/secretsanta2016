b626857b814a58efee9517adeffff101ca75cea3
Anon Ymous <Anon Ymous>
2016-12-23 15:43:47 -0500

    Started painting

--- game.js
+++ game.js
@@ -1,3 +1,93 @@
+// == INITIALIZE ==
+// ================
+
 function init(){
-	console.log('init');
+	C().init();
 }
+
+
+
+
+
+
+
+
+
+
+// == CANVAS ==
+// ============
+var Canvas={
+	// == FUNCTIONS ==
+	// ===============
+	
+	init:function(){
+		// Set the canvas-related variables
+		C().c=document.getElementById('c');
+		C().d=C().c.getContext('2d');
+		C().loop=setInterval(function(){
+			if(C().paused) return;
+			C().step();
+			C().draw();
+		},1000/60);
+		C().sizeAndPosition();
+		window.addEventListener('resize',function(){C().sizeAndPosition()});
+		
+		// Make the game's code all pause when you aren't focused on this screen
+		window.addEventListener('blur',function(){
+			C().paused=true;
+		});
+		window.addEventListener('focus',function(){
+			C().paused=false;
+		});
+	},
+	
+	step:function(){
+		// Nothing yet
+	},
+	
+	draw:function(){
+		C().d.clearRect(
+			0,
+			0,
+			C().c.width,
+			C().c.height
+		);
+		
+		C().d.fillRect(
+			Math.round(Math.random() * 10),
+			Math.round(Math.random() * 10),
+			64,
+			64
+		);
+	},
+	
+	sizeAndPosition:function(){
+		C().c.width=C().size.width;
+		C().c.height=C().size.height;
+		C().c.style.position='relative';
+		C().c.style.left=(window.innerWidth - C().c.width) / 2 + 'px';
+		C().c.style.top=(window.innerHeight - C().c.height) / 2 + 'px';
+	},
+	
+	
+	
+	
+	
+	// == VARIABLES ==
+	// ===============
+	
+	// The canvas element
+	c:null,
+	// The canvas context, used for drawing
+	d:null,
+	// The game loop
+	loop:null,
+	// Whether or not the game window is paused. This makes the game do not code; this is not like a pause button
+	paused:false,
+	// The virtual size of the screen
+	size:{
+		width:640,
+		height:480
+	}
+};
+function C(){return window.Canvas;}
